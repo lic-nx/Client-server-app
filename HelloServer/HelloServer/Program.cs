7e92498294efca260e476a1f9fc93f926f8f925d
lic-nx <lic-nx@yandex.ru>
2022-02-08 13:23:42 +0300

    Добавьте файлы проекта.

---
+++ HelloServer/HelloServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using RemoteHello;
+namespace HelloServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создание канала
+            TcpServerChannel channel = new TcpServerChannel(8086);
+            //Регистация канала
+            ChannelServices.RegisterChannel(channel, false);
+            //Регистрация хорошо известного объекта
+            RemotingConfiguration.RegisterWellKnownServiceType(
+            typeof(Hello),
+            "Hi",
+            WellKnownObjectMode.SingleCall);
+            Console.WriteLine("Сервер работает");
+            Console.WriteLine("Нажмите Enter для выхода");
+            Console.ReadLine();//Удерживаем сервер в рабочем состоянии
+        }
+    }
+}
---
+++ RemoteHello/RemoteHello/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace RemoteHello
+{
+    public class Hello : MarshalByRefObject
+    {
+        public Hello()
+        {
+            Console.WriteLine("Объект Hello создан");
+        }
+        ~Hello()
+        {
+            Console.WriteLine("Объект Hello уничтожен");
+        }
+        public string Greeting(string name)
+        {
+            Console.WriteLine("Метод Greeting вызван");
+            return "Приветствую Вас, " + name;
+        }
+    }
+}
